797d16fbfe434534fc5bd7ef5aa103e1d19d21dd
bsmuse <bsmuse@tutamail.com>
2022-06-02 08:59:40 -0600

    Modifiying HTML and CSS Add styling to css and modify HTML Input field to limit characters.

---
+++ index.js
@@ -0,0 +1,11 @@
+let inputEl = document.getElementById("input-el")
+
+
+1 Metre/s = 3.2808399 Feet 
+1 Feet = 0.3048 Metre/s 
+
+1 Litre/s = 0.219968813 Gallon/s 
+1 Gallon/s = 4.546099 Litre/s 
+
+1 Pound/s = 0.45359237 Kilogram/s
+1 Kilogram/s = 2.20462262 Pound/s
