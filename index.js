51c9328dd46f60ab6e6b026cc86a7e463884ae3c
bsmuse <bsmuse@tutamail.com>
2022-06-05 08:00:14 -0600

    Finish Project Figure out that JS need ID vs Class selector and learn addEventListener method.

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-let inputEl = document.getElementById("input-el")
+input.addEventListener("input",conversion)
 
+function conversion (e){
 
-1 Metre/s = 3.2808399 Feet 
-1 Feet = 0.3048 Metre/s 
+    number= e.target.value
+    
+    meters1.textContent = feet1.textContent = liters1.textContent = gallons1.textContent = kilos1.textContent = pounds1.textContent  = number
 
-1 Litre/s = 0.219968813 Gallon/s 
-1 Gallon/s = 4.546099 Litre/s 
-
-1 Pound/s = 0.45359237 Kilogram/s
-1 Kilogram/s = 2.20462262 Pound/s
+    feet2.textContent= (number * 3.28084).toFixed(3)
+    meters2.textContent= (number / 3.2808).toFixed(3)
+    gallons2.textContent =(number * 0.26417205235815).toFixed(3)
+    liters2.textContent =(number * 3.785412).toFixed(3)
+    pounds2.textContent =(number * 2.20462).toFixed(3)
+    kilos2.textContent =(number * 0.453592).toFixed(3)
+}
